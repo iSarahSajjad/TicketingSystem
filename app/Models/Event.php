33bbbd1198d037bfe1c3782d494d487dc4231c6f
Sarah <sarahsajjad93@gmail.com>
2022-05-28 16:53:23 +0500

    statup passport for auth and event model added

---
+++ app/Models/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'lineup',
+        'maximum_tickets',
+        'user_id'
+    ];
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+}
